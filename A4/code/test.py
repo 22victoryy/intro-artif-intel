0d153521bd7b0a5bd4bae4360395b0fa9865dfc1
vchanyoung <dk90801@gmail.com>
2020-04-02 19:16:16 -0400

    final?

--- A4/code/test.py
+++
@@ -1,9 +0,0 @@
-for f in factors:
-    restrict_vars = v_intersect.intersection(f.get_scope())
-    if len(restrict_vars) > 0:  # f can be restricted
-        ff = f
-        for ef in restrict_vars:
-            ff = restrict_factor(ff, ef, ef.get_evidence())
-        evar_list.append(ff)
-    else:
-        evar_list.append(f)
